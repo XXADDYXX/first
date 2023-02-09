dae846fce9f39f3b5e417fc826f083142daf1185
Aadarsh Mane <onlyaddy68@gmail.com>
2023-02-10 00:21:52 +0530

    intial commit

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff{
+    public static void main(String[] args) {
+        int a=10;
+        int b=8;
+        int diff=a-b;
+        System.out.println("thaxaxiis"+diff);
+    }
+    }

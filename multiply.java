1892270b63c1f973a17e0480d7a42f86449bd3b0
Aadarsh Mane <onlyaddy68@gmail.com>
2023-02-10 01:12:39 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply{
+    public static void main(String[] args) {
+        int a=10;
+        int b=8;
+        int diff=a*b;
+        System.out.println("thaxaxiis"+diff);
+    }
+    }
